cd497cb7ea6173434ab6daa76da82c5d1e08c67e
Anastasiia <anastasiia14merk@gmail.com>
2021-11-30 00:01:48 +0200

    questions

---
+++ js/style.js
@@ -0,0 +1,22 @@
+"use sctrict";
+const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели"," ");
+console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: true||false,
+};
+
+const a= prompt ('Один из последних просмотренных фильмов?', '');
+      b= prompt('На сколько оцение его?', '');
+      c= prompt('Один из последних просмотренных фильмов?', '');
+      d= prompt('На сколько оцение его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
